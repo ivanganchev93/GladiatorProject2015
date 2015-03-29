48697a8d4bc3cd3b84c67a634d42cc66c2578a11
alex <alex9407@abv.bg>
2015-03-29 14:48:57 +0100

    jquery and templateTags comments

--- GladiatorTeamProject/Spartacus/templatetags/app_filters.py
+++ GladiatorTeamProject/Spartacus/templatetags/app_filters.py
@@ -5,10 +5,22 @@ register = template.Library()
 
 @register.filter(name='mulLeft')
 def mulLeft(value,value2):
+    """
+    multiply function used for css position of avatar's invetory items
+    :param value:
+    :param value2:
+    :return:
+    """
     return (value%4)*value2
 
 @register.filter(name='mulTop')
 def mulTop(value,value2):
+    """
+    multiply function used for css position of avatar's invetory items
+    :param value:
+    :param value2:
+    :return:
+    """
     if value < 4:
         return 1*value2
     else:
--- GladiatorTeamProject/static/js/ajax.js
+++ GladiatorTeamProject/static/js/ajax.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-    
+
+    //Called if equip id is clicked, equipping the item with particular id
     $('.equip').click(function(){
         var itemid = $(this).attr("data-itemid");
         $.get("/Spartacus/equip_item/", {item_id: itemid}, function(data){
@@ -7,6 +8,7 @@ $(document).ready(function() {
         });
     });
 
+    //Called if unequip id is clicked, unequipping the item with particular id
     $('.unequip').click(function(){
         var itemid = $(this).attr("data-itemid");
         $.get("/Spartacus/unequip_item/", {item_id: itemid}, function(data){
@@ -14,6 +16,7 @@ $(document).ready(function() {
         });
     });
 
+    //Called if unequip id is clicked, unequipping the item with particular id in the market template
     $('.unequip_market').click(function(){
         var itemid = $(this).attr("data-itemid");
         $.get("/Spartacus/unequip_item_market/", {item_id: itemid}, function(data) {
@@ -22,6 +25,7 @@ $(document).ready(function() {
         });
     });
 
+    //Called if sell id is clicked, sell the item with particular id
     $('.sell').click(function(){
         var itemid = $(this).attr("data-itemid");
 
